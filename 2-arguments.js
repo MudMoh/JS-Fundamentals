695f732d892d550402e40fffde55980b11dd1471
MudMoh <mudesir@gmail.com>
2025-05-20 23:54:23 +0300

    prints a message depending of the number of arguments passed

---
+++ 2-arguments.js
@@ -0,0 +1,8 @@
+// console.log(process.argv.length);
+if (process.argv.length < 3) {
+    console.log('No argument');
+} else if (process.argv.length === 3) {
+    console.log('Argument found');
+} else {
+    console.log('Arguments found');
+}  
