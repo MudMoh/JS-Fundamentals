eab26e5ff7b0080ea82fb82a57d7906f0c2f796d
MudMoh <mudesir@gmail.com>
2025-05-21 00:58:33 +0300

    using repeat() js function

--- 8-square.js
+++ 8-square.js
@@ -1,6 +1,6 @@
 const num = parseInt(process.argv[2]);
 if (isNaN(num)) {
-    console.log('Missing sizer');
+    console.log('Missing size');
 } else {
     for (let i = 0; i < num; i++) {
         console.log("X".repeat(num));
